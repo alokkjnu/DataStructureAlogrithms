91f3df17c416a7c1ec197615b1c3072625c0ceab
Alok <ry7069577@gmail.com>
2025-10-30 12:09:00 +0530

    updated code

--- DSA/8 CalculatingWithDictionaries.py
+++ DSA/8 CalculatingWithDictionaries.py
@@ -19,7 +19,6 @@ max_price = max(zip(price.values(),price.keys()))
 print(max_price)
 
 price_sorted = sorted(zip(price.values(),price.keys()))
-
 print(price_sorted)
 
 price_and_names = zip(price.values(),price.keys())
