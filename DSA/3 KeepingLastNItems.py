79098610bc3e1922fbe9093a2077b2d0074b6170
Alok <ry7069577@gmail.com>
2025-11-02 12:01:00 +0530

    updated code

--- DSA/3 KeepingLastNItems.py
+++ DSA/3 KeepingLastNItems.py
@@ -7,13 +7,14 @@ from collections import deque
 def search(lines, pattern, history=5):
 
     previous_line = deque(maxlen=history)
-    
+
     for line in lines:
         if pattern in line:
             yield line, previous_line
 
 
 if __name__ == '__main__':
+    
     with open('msg.txt') as f:
         for line, prevline in search(f, 'Python', 5):
             for pline in prevline:
