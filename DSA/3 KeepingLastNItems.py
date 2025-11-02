80329a8aadb3e5afd82589ad2524c72ce91a6f46
Alok <ry7069577@gmail.com>
2025-11-02 12:01:00 +0530

    updated code

--- DSA/3 KeepingLastNItems.py
+++ DSA/3 KeepingLastNItems.py
@@ -5,8 +5,9 @@ from collections import deque
 
 
 def search(lines, pattern, history=5):
-    
+
     previous_line = deque(maxlen=history)
+    
     for line in lines:
         if pattern in line:
             yield line, previous_line
