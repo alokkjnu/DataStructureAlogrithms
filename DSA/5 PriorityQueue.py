e1f8f26a3ee7624ef7da59dcf274ccaa123691d5
Alok <ry7069577@gmail.com>
2025-12-13 12:03:00 +0530

    updated code

--- DSA/5 PriorityQueue.py
+++ DSA/5 PriorityQueue.py
@@ -5,10 +5,11 @@ Implementing a Priority Queue
 import heapq
 
 class PriorityQueue:
-    
+
     def __init__(self):
         self._queue = []
         self._index = 0
+        
     def push(self,item,priority):
         heapq.heappush(self._queue,(-priority,self._index,item))
         self._index +=1
