8357b49ce7acf1c924f367a0e8486ab606d4ddd5
Alok Kumar Maurya <ry7069577@gmail.com>
2026-03-06 20:41:34 +0530

    Your commit message

--- DSA/10 RemovingDulicates.py
+++ DSA/10 RemovingDulicates.py
@@ -20,8 +20,9 @@ b = list(dedupe(a))
 print(b)
 
 def dedupe(items,key=None):
-    
+
     seen = set()
+    
     for item in items:
         val = item if key is None else key(item)
         if val not in seen:
